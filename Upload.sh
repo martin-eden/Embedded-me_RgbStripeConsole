d37e8ea910e66b0d68011772fc064c431c0147ed
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-09-19 22:25:18 +0200

    [.^] Better file naming and script code fro uploader.

--- Upload.sh
+++ Upload.sh
@@ -3,18 +3,21 @@
 # Just upload compiled binary
 
 # Author: Martin Eden
-# Last mod.: 2024-10-13
+# Last mod.: 2025-09-19
 
 # "Works on my machine.."
 
 avrdude \
-  -C bin/avrdude.conf \
-  -p atmega328p \
-  -c arduino \
+  -U flash:w:bin/me_RgbStripeConsole.ino.hex:i \
   -P /dev/ttyUSB0 \
   -b 115200 \
+  -C bin/ATmega328.avrdude.conf \
+  -p atmega328p \
+  -c arduino \
   -D \
-  -U flash:w:bin/me_RgbStripeConsole.ino.hex:i \
   -qq
 
+echo "Code loaded to board."
+
 # 2024-10-13
+# 2025-09-19
